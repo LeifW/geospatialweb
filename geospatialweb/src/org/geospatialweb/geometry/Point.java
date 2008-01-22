45679b89e17569d11fafea211463f8c43893ced5
marco.neumann <marco.neumann@4135a6d1-d843-0410-a865-790d70d7ecba>
2008-01-22 01:37:18 +0000

    3D Point
    git-svn-id: http://geospatialweb.googlecode.com/svn/trunk@14 4135a6d1-d843-0410-a865-790d70d7ecba

---
+++ geospatialweb/src/org/geospatialweb/geometry/Point.java
@@ -0,0 +1,32 @@
+package org.geospatialweb.geometry;
+
+public class Point {
+	double x;
+	double y;
+	double z;
+	
+	void setX(double x){
+		this.x = x;
+	}
+	
+	void setY(double y){
+		this.y = y;
+	}
+	
+	void setZ(double z){
+		this.z = z;
+	}
+	
+	double getX(){
+		return x;
+	}
+	
+	double getY(){
+		return y;
+	}
+
+	double getZ(){
+		return z;
+	}
+
+}
