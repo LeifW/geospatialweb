29b9d3f285974dbe9be557de3f11bf3d6611df55
thewebsemantic <thewebsemantic@4135a6d1-d843-0410-a865-790d70d7ecba>
2008-02-01 21:48:18 +0000

    git-svn-id: http://geospatialweb.googlecode.com/svn/trunk@44 4135a6d1-d843-0410-a865-790d70d7ecba

---
+++ jenaext/src/org/geospatialweb/arqext/distance.java
@@ -0,0 +1,23 @@
+package org.geospatialweb.arqext;
+
+import org.geospatialweb.geometry.GeometryFunction;
+
+import com.hp.hpl.jena.sparql.expr.NodeValue;
+import com.hp.hpl.jena.sparql.function.FunctionBase4;
+
+public class distance extends FunctionBase4 {
+
+	@Override
+	public NodeValue exec(NodeValue v1, NodeValue v2, NodeValue v3, NodeValue v4) {
+		double lat = v1.getDouble();
+		double lon = v2.getDouble();
+		double lat2 = v3.getDouble();
+		double lon2 = v4.getDouble();
+		
+		GeometryFunction geo = new GeometryFunction();
+		geo.getDistance2D(lat, lon, lat2, lon2);
+		return null;
+	}
+
+
+}
