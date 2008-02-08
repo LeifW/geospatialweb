4adc7b98d735601c976a30139992144c36fae10c
thewebsemantic <thewebsemantic@4135a6d1-d843-0410-a865-790d70d7ecba>
2008-02-08 18:26:36 +0000

    git-svn-id: http://geospatialweb.googlecode.com/svn/trunk@63 4135a6d1-d843-0410-a865-790d70d7ecba

---
+++ jenaext/example/ex/ClassifyGeodata.java
@@ -0,0 +1,67 @@
+package ex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+
+public class ClassifyGeodata {
+	static String AIRPORT = "AIRP";
+	static String HOTEL = "HTL";
+	
+	public static OntModel getModel(String filepath) throws IOException {
+		String geo = "http://www.w3.org/2003/01/geo/wgs84_pos#";
+		String base = "http://example.org/";
+
+		OntModel m = ModelFactory.createOntologyModel();
+
+		OntClass city = m.createClass(base + "City");
+		OntClass hotel = m.createClass(base + "Hotel");
+		OntClass airport = m.createClass(base + "Airport");
+
+		Property plat = m.createProperty(geo + "lat");
+		Property plon = m.createProperty(geo + "long");
+		Property pname = m.createProperty(base + "name");
+
+		FileReader file = new FileReader(filepath);
+
+		BufferedReader reader = new BufferedReader(file);
+		reader.readLine();
+		int i=0;
+		for (String line; (line = reader.readLine()) != null;) {
+			String[] values = line.split("\t");
+			String id = values[0];
+			String name = values[1];
+			double lat = Double.parseDouble(values[4]);
+			double lon = Double.parseDouble(values[5]);
+			String subtype = values[7];
+			int population = Integer.parseInt( values[14] );
+
+				
+			Individual r = null;
+			if (subtype.startsWith("PPL") && population > 0) {
+				r = city.createIndividual(base + id);
+				//System.out.println(population);
+			} else if (HOTEL.equals(subtype)) {
+				r = hotel.createIndividual(base + id);
+			}else if (AIRPORT.equals(subtype)) {
+				r = airport.createIndividual(base + id);
+			} else { 
+				continue;
+			}
+			
+			r.addProperty(plat, m.createTypedLiteral(lat));
+			r.addProperty(plon, m.createTypedLiteral(lon));
+			r.addProperty(pname, name);
+			if ( name.equals("Marlow"))
+				System.out.println(r.getRDFType());
+
+		} 
+		return m;
+	}
+}
--- jenaext/example/ex/TestBasic.java
+++ jenaext/example/ex/TestBasic.java
@@ -1,27 +1,24 @@
 package ex;
+
 /**
  * You'll need more that just the libs in jenalib.  jenalib has the bare minimum to
  * compile, however, to run this you need the full ARQ distribution set of libraries.
  * 
  */
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.geospatialweb.arqext.Geo;
+import org.geospatialweb.arqext.Indexer;
+import org.geospatialweb.geometry.GeometryFunction;
+import org.junit.Test;
+
 import spatialindex.SpatialIndex;
 import spatialindex.rtree.RTree;
 import spatialindex.storagemanager.IStorageManager;
 import spatialindex.storagemanager.PropertySet;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-import org.geospatialweb.arqext.Geo;
-import org.geospatialweb.arqext.Indexer;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-
-
-
-import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
@@ -31,84 +28,66 @@ import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.Resource;
 
 public class TestBasic {
-	
 
-
-
+	@Test
 	public void england() throws IOException {
 		System.out.println("Loading large file into jena model...");
-		String geo = "http://www.w3.org/2003/01/geo/wgs84_pos#";
-		String base = "http://example.org/";
-
-		OntModel m = ModelFactory.createOntologyModel();
-		OntClass city =  m.createClass(base + "City");
-		OntClass other =  m.createClass(base + "Other");
-		Property plat = m.createProperty(geo + "lat");
-		Property plon = m.createProperty(geo + "long");
-		Property pname = m.createProperty(base + "name");
-
-		
-		FileReader file = new FileReader("jenaext/GB.txt");
-		BufferedReader reader = new BufferedReader(file);
-		reader.readLine();
-		for (String line; (line = reader.readLine()) != null;) {
-			String[] values = line.split("\t");
-			String id = values[0];
-			String name = values[1];
-			double lat = Double.parseDouble(values[4]);
-			double lon = Double.parseDouble(values[5]);
-			String type = values[6];
-
-			Resource r = ("P".equals(type)) ?
-					m.createIndividual(base + id, city):
-					m.createIndividual(base + id, other);
-			r.addProperty(plat, m.createTypedLiteral(lat));
-			r.addProperty(plon, m.createTypedLiteral(lon));
-			r.addProperty(pname, name);
-
-		}
-		//m.write(System.out);
-		
+		long t1 = System.currentTimeMillis();
+		OntModel m = ClassifyGeodata.getModel("jenaext/GB.txt");
+		long t2 = System.currentTimeMillis();
+		System.out.println(t2 -t1);
 		IStorageManager store = SpatialIndex.createMemoryStorageManager();
 		RTree rtree = new RTree(props(), store);
 		Indexer i = new Indexer(rtree);
 		i.createIndex(m);
+		System.out.println("Index size: " + i.getSize());
 
-		String queryString = 
-			"PREFIX : <http://example.org/>" +	
-			"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n\n " +
-			"SELECT ?n ?lat ?lon WHERE { ?s a :City . ?s :name ?n . ?s geo:lat ?lat . ?s geo:long ?lon}";
+		String queryString = "PREFIX : <http://example.org/>"
+				+ "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n\n "
+				+ "SELECT ?n ?lat ?lon WHERE { ?s a :City . ?s :name ?n . ?s geo:lat ?lat . ?s geo:long ?lon}";
+
+		queryString = "PREFIX : <http://example.org/>"
+				+ "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n "
+				+ "PREFIX ext: <java:org.geospatialweb.arqext.>\n\n "
+				+ "SELECT ?city ?hotel ?lat1 ?lon1 ?lat2 ?lon2 WHERE { ?c a :City . ?s ext:nearby ?c . ?s :name ?hotel . ?c :name ?city . ?s a :Hotel "
+				+ " . ?c geo:lat ?lat1 . ?c geo:long ?lon1 . ?s geo:lat ?lat2 . ?s geo:long ?lon2}";
 
 		// First get a list of cities to find nearby things
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.create(query, m);
+		Geo.setContext(qexec, i);
+
 		try {
 			ResultSet results = qexec.execSelect();
-			for (;results.hasNext();) {
+			for (; results.hasNext();) {
 				QuerySolution soln = results.nextSolution();
-				double lat =  soln.getLiteral("lat").getDouble();
-				double lon = soln.getLiteral("lon").getDouble();
-				String place = soln.getLiteral("n").getString();				
-				findNearbyStuff(m, i, place, lat, lon);
+				
+				double lat1 = soln.getLiteral("lat1").getDouble();
+				double lon1 = soln.getLiteral("lon1").getDouble();
+				double lat2 = soln.getLiteral("lat2").getDouble();
+				double lon2 = soln.getLiteral("lon2").getDouble();
+				String city = soln.getLiteral("city").getString();
+				String hotel = soln.getLiteral("hotel").getString();
+				double dist = GeometryFunction.getDistanceSpheroid(lat1, lon1, lat2, lon2);
+				System.out.print(city + " " + hotel  + " " + dist + "\n");
+				// String place = soln.getLiteral("n").getString();
+				// findNearbyStuff(m, i, place, lat, lon);
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			qexec.close();
 		}
-		
-
-		
-
 	}
 
-	private void findNearbyStuff(OntModel m, Indexer i, String city, double lat, double lon) {
-		String queryString = 
-			"PREFIX : <http://example.org/>" +			
-			"PREFIX geo: <java:org.geospatialweb.arqext.>\n\n " +
-			"SELECT ?n WHERE { ?s geo:nearby(" + lat + " " + lon + " 10) . ?s a :Other . ?s :name ?n }";
+	private void findNearbyStuff(OntModel m, Indexer i, String city,
+			double lat, double lon) {
+		String queryString = "PREFIX : <http://example.org/>"
+				+ "PREFIX geo: <java:org.geospatialweb.arqext.>\n\n "
+				+ "SELECT ?n WHERE { ?s geo:nearby(" + lat + " " + lon
+				+ " 10) . ?s a :Other . ?s :name ?n }";
 
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.create(query, m);
@@ -117,16 +96,17 @@ public class TestBasic {
 			long t1 = System.currentTimeMillis();
 			ResultSet results = qexec.execSelect();
 			long t2 = System.currentTimeMillis();
-			System.out.println("Query complete in " + (t2-t1) + "ms");
+			System.out.println("Query complete in " + (t2 - t1) + "ms");
 			System.out.println("Places near " + city);
-			for (;results.hasNext();) {
-				System.out.println("\t" + results.nextSolution().getLiteral("n"));				
+			for (; results.hasNext();) {
+				System.out.println("\t"
+						+ results.nextSolution().getLiteral("n"));
 			}
 		} finally {
 			qexec.close();
 		}
 	}
-	
+
 	@Test
 	public void testWithin() {
 		Model m = ModelFactory.createDefaultModel();
@@ -135,10 +115,9 @@ public class TestBasic {
 		RTree rtree = new RTree(props(), store);
 		Indexer i = new Indexer(rtree);
 		i.createIndex(m);
-		String queryString = 
-			"PREFIX : <http://www.geonames.org/ontology#>" +			
-			"PREFIX geo: <java:org.geospatialweb.arqext.>\n\n " +
-			"SELECT ?n WHERE { ?s geo:contains(38.18 -9.78 53.75 22.41 ) . ?s :name ?n }";
+		String queryString = "PREFIX : <http://www.geonames.org/ontology#>"
+				+ "PREFIX geo: <java:org.geospatialweb.arqext.>\n\n "
+				+ "SELECT ?n WHERE { ?s geo:contains(38.18 -9.78 53.75 22.41 ) . ?s :name ?n }";
 
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.create(query, m);
@@ -147,20 +126,20 @@ public class TestBasic {
 			long t1 = System.currentTimeMillis();
 			ResultSet results = qexec.execSelect();
 			long t2 = System.currentTimeMillis();
-			System.out.println(t2-t1);
-			for (;results.hasNext();) {
+			System.out.println(t2 - t1);
+			for (; results.hasNext();) {
 				System.out.println(results.nextSolution().getLiteral("n"));
-				
+
 			}
 		} finally {
 			qexec.close();
 		}
-		
+
 	}
 
 	@Test
 	public void testGood() {
-		
+
 		Model m = ModelFactory.createDefaultModel();
 		m.read("file:jenaext/capitals.rdf");
 		IStorageManager store = SpatialIndex.createMemoryStorageManager();
@@ -168,11 +147,9 @@ public class TestBasic {
 		Indexer i = new Indexer(rtree);
 		i.createIndex(m);
 
-		
-		String queryString = 
-			"PREFIX : <http://www.geonames.org/ontology#>" +			
-			"PREFIX geo: <java:org.geospatialweb.arqext.>\n\n " +
-			"SELECT ?n WHERE { ?s geo:nearby(19 -99 10) . ?s :name ?n }";
+		String queryString = "PREFIX : <http://www.geonames.org/ontology#>"
+				+ "PREFIX geo: <java:org.geospatialweb.arqext.>\n\n "
+				+ "SELECT ?n WHERE { ?s geo:nearby(19 -99 10) . ?s :name ?n }";
 
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.create(query, m);
@@ -181,20 +158,20 @@ public class TestBasic {
 			long t1 = System.currentTimeMillis();
 			ResultSet results = qexec.execSelect();
 			long t2 = System.currentTimeMillis();
-			System.out.println(t2-t1);
-			for (;results.hasNext();) {
+			System.out.println(t2 - t1);
+			for (; results.hasNext();) {
 				System.out.println(results.nextSolution().getLiteral("n"));
-				
+
 			}
 		} finally {
 			qexec.close();
 		}
 
 	}
-	
+
 	@Test
 	public void testError() {
-		
+
 		Model m = ModelFactory.createDefaultModel();
 		m.read("file:jenaext/capitals.rdf");
 		IStorageManager store = SpatialIndex.createMemoryStorageManager();
@@ -202,11 +179,9 @@ public class TestBasic {
 		Indexer i = new Indexer(rtree);
 		i.createIndex(m);
 
-		
-		String queryString = 
-			"PREFIX : <http://www.geonames.org/ontology#>" +			
-			"PREFIX geo: <java:org.geospatialweb.arqext.>\n\n " +
-			"SELECT ?n WHERE { ?s :name ?n . ?s geo:nearby(19 -99 10)  }";
+		String queryString = "PREFIX : <http://www.geonames.org/ontology#>"
+				+ "PREFIX geo: <java:org.geospatialweb.arqext.>\n\n "
+				+ "SELECT ?n WHERE { ?s :name ?n . ?s geo:nearby(19 -99 10)  }";
 
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.create(query, m);
@@ -216,13 +191,13 @@ public class TestBasic {
 			long t1 = System.currentTimeMillis();
 			ResultSet results = qexec.execSelect();
 			long t2 = System.currentTimeMillis();
-			System.out.println(t2-t1);
-			for (;results.hasNext();) {
+			System.out.println(t2 - t1);
+			for (; results.hasNext();) {
 				System.out.println(results.nextSolution().getLiteral("n"));
-				
+
 			}
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			caught = true;
 		} finally {
 			qexec.close();
@@ -230,14 +205,10 @@ public class TestBasic {
 		assertTrue(caught);
 
 	}
+
 	private static PropertySet props() {
 		PropertySet ps2 = new PropertySet();
-		Double f = new Double(0.7);
-		ps2.setProperty("FillFactor", f);
-		ps2.setProperty("IndexCapacity", 100);
-		ps2.setProperty("LeafCapacity", 100);
-			// Index capacity and leaf capacity may be different.
 		ps2.setProperty("Dimension", 2);
 		return ps2;
-	}	
+	}
 }
