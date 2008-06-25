0e54f8e37b46b67d12e6f4316c360c307adccc09
thewebsemantic <thewebsemantic@4135a6d1-d843-0410-a865-790d70d7ecba>
2008-06-25 02:56:55 +0000

    git-svn-id: http://geospatialweb.googlecode.com/svn/trunk@103 4135a6d1-d843-0410-a865-790d70d7ecba

--- geohash/src/Encoder.java
+++ geohash/src/Encoder.java
@@ -1,19 +1,9 @@
 import java.util.BitSet;
-import java.util.HashMap;
 
 public class Encoder {
 
 	private static int numbits = 6 * 5;
-	final static char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
-			'9', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p',
-			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-	
-	final static HashMap<Character, Integer> lookup = new HashMap<Character, Integer>();
-	static {
-		int i = 0;
-		for (char c : digits)
-			lookup.put(c, i++);
-	}
+
 
 	public static void main(String[] args) {
 		double[] latlon = new Encoder().decode("dj248j248j24");
@@ -28,7 +18,7 @@ public class Encoder {
 		StringBuilder buffer = new StringBuilder();
 		for (char c : geohash.toCharArray()) {
 
-			int i = lookup.get(c) + 32;
+			int i = Base32.lookup.get(c) + 32;
 			buffer.append( Integer.toString(i, 2).substring(1) );
 		}
 		
@@ -80,7 +70,7 @@ public class Encoder {
 			buffer.append( (lonbits.get(i))?'1':'0');
 			buffer.append( (latbits.get(i))?'1':'0');
 		}
-		return base32(Long.parseLong(buffer.toString(), 2));
+		return Base32.base32(Long.parseLong(buffer.toString(), 2));
 	}
 
 	private BitSet getBits(double lat, double floor, double ceiling) {
@@ -97,21 +87,6 @@ public class Encoder {
 		return buffer;
 	}
 
-	public static String base32(long i) {
-		char[] buf = new char[65];
-		int charPos = 64;
-		boolean negative = (i < 0);
-		if (!negative)
-			i = -i;
-		while (i <= -32) {
-			buf[charPos--] = digits[(int) (-(i % 32))];
-			i /= 32;
-		}
-		buf[charPos] = digits[(int) (-i)];
 
-		if (negative)
-			buf[--charPos] = '-';
-		return new String(buf, charPos, (65 - charPos));
-	}
 
 }
